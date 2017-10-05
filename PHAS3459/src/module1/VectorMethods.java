cb5e600db04d5c4411dfc4cfa88ba540fcf0796e
zcapoma <zcapoma@ucl.ac.uk>
2017-10-05 11:41:37 +0100

    Final VectorMethods with comments

--- PHAS3459/src/module1/VectorMethods.java
+++ PHAS3459/src/module1/VectorMethods.java
@@ -11,14 +11,14 @@ public class VectorMethods {
 		     return mag;
 		     
 		 }
-	
+	// writing the dotProduct function for two 3-D vectors
 	public double dotProduct(double x1,double y1,double z1, double x2, double y2, double z2) {
 		double dotprod;
 		dotprod = x1*x2 + y1*y2 + z1*z2; // calculates dot product for two 3-D vectors
 		return dotprod;
 		
 		 }
-		
+	// writing the angle function to find the angle between two 3-D vectors	
 	public double angle(double x1,double y1,double z1, double x2, double y2, double z2) {
 			double ang;
 			// calculates angle between two 3-D vectors
@@ -37,15 +37,20 @@ public class VectorMethods {
 		// calling dot product function
 		double scalarprod = vm.dotProduct(1, 2, 3, 4, 5, 6);
 		System.out.println("The scalar product of the vectors (1,2,3) and (4,5,6) is "+scalarprod);
+		System.out.println();
 		
 		// calling angle function to calculate angle between two vectors
 		// angle 1 between vectors (2,3,4) and (1,3,2)
 		double angle1 = vm.angle(2, 3, 4, 1, 3, 2);
 		System.out.println("The angle between vectors (2,3,4) and (1,3,2) is "+angle1 +" radians");
+		System.out.println();
 		
 		// angle 2 between vectors (2,3,4) and (0,0,0)
 		double angle2 = vm.angle(2, 3, 4, 0, 0, 0);
 		System.out.println("The angle between vectors (2,3,4) and (0,0,0) is "+angle2 +" radians");
+		// this does not output a number due to division by zero; (0,0,0) is a null vector
+		System.out.println("The above returned NaN due to division by zero, from the magnitude of the null vector.");
+	
 	}
 
 }
