bda8c41cae89c6391071eef0917ad14cd79f9f4f
Ohie Mayenin <zcapoma@ucl.ac.uk>
2017-10-25 13:05:00 +0100

    Module 4 WordCounter - end of first session

--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -9,31 +9,41 @@ public class WordCounter {
 		InputStream is_url = u.openStream(); // inputs URL as bytes
 		InputStreamReader isr_url = new InputStreamReader(is_url); // wrap input stream
 		BufferedReader url = new BufferedReader(isr_url); // reads large chunk of data into memory
-//		String line;
-//		while ((line = url.readLine()) != null) {
-//			 System.out.println(line);
-//			 } 
+		//		String line;
+		//		while ((line = url.readLine()) != null) {
+		//			 System.out.println(line);
+		//			 } 
 		return url;
 	}
-	
+
 	public static BufferedReader brFromFile(String fileName) throws IOException {
 		FileReader file = new FileReader(fileName);
 		BufferedReader buffer_file = new BufferedReader(file); 
 		return buffer_file;
 	}
-	
-	//public static int countWordsInResource(BufferedReader dataAsBR) {}
-	
-	
+
+	public static int countWordsInResource(BufferedReader dataAsBR) {
+		Scanner s = new Scanner(dataAsBR);
+		int sum = 0;
+		while (s.hasNext()) {
+			sum = sum + 1;	
+		}
+
+		return sum;
+
+	}
+
+
 	public static void main(String[] args) {
 		try {
-			brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
+			BufferedReader br_passage = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
+			System.out.println("Number of words in passage: "+countWordsInResource(br_passage));
 		}
 		catch (Exception e) {
 		}
-		 }
-	
-	
+	}
+
+
 }
 
 
