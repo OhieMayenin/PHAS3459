e7022e514152b8655b9075cb779a962640045f5d
Ohie Mayenin <zcapoma@ucl.ac.uk>
2017-10-26 13:10:03 +0100

    Numerical reader class not complete, end of thursday session.

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -4,6 +4,9 @@ import java.net.*;
 import java.util.*;
 
 public class NumericalReader {
+	PrintWriter pw;
+	double minValue, maxValue, sumOfValues;
+	int nValues;
 	
 	public static String getStringFromKeyboard() throws IOException{
 		InputStreamReader input = new InputStreamReader(System.in);
@@ -12,7 +15,7 @@ public class NumericalReader {
 		String s = b.readLine();
 		return s;
 	}
-	
+
 	public BufferedReader brFromURL(String urlName) throws IOException {
 		URL u = new URL(urlName); // convert string to URL
 		InputStream is_url = u.openStream(); // inputs URL as bytes
@@ -20,41 +23,68 @@ public class NumericalReader {
 		BufferedReader url = new BufferedReader(isr_url); // reads large chunk of data into memory
 		return url;
 	}
-	
+
 	// method creates file
-	static void analysisStart(String dataFile) throws IOException {
-		String file = ("N:" + File.separator + "EclipseFiles" + File.separator + dataFile);
+	static void analysisStart(String dataFile, String directory) throws IOException {
+		String file = ("N:" + File.separator + directory + File.separator + dataFile);
 		File outputfile = new File(file);
-		FileWriter fw = new FileWriter(outputfile);
-				
-//		private double minValue;
-//		private double maxValue;
-//		private double nValues;
-//		private double sumOfValues;
-	}
-	
-	void analyseData(String line) {
-		if (line.isEmpty()) {
-			//line.next();
-		}
-	}
-	
-	void analysisEnd() {
+		FileWriter fw = new FileWriter(outputfile); //N:\\numbers1.txt
+
+		double minValue = Double.MIN_VALUE;
+		double maxValue = Double.MAX_VALUE;
+		int nValues = 0;
+		double sumOfValues = 0;
 		
 	}
-	
-	
-	public static void main(String[] args) {
-		try{ String inputString = getStringFromKeyboard();
-		System.out.println("Directory where data is to be stored: "+inputString);
-		analysisStart(inputString);
+
+	void analyseData(String line) throws IOException{
+		if (line.isEmpty() || Character.isLetter(line.charAt(0))) {
+			return; 
 		}
+			PrintWriter pw = new PrintWriter("numbers1.txt");
+			Scanner s = new Scanner(line);
+			while (s.hasNext()) {
+				
+			}
+			System.out.println(pw);
+			pw.close();
+	}
+
+		
+		
+	void analysisEnd() {
+
+	}
+
+
+	public static void main(String[] args) {
+		
+		// creating and storing data into numbers1.txt file
+		String line = "";
+		NumericalReader nr =  new NumericalReader();
+		
+		try{ String directory = getStringFromKeyboard();
+		System.out.println("Directory where data is to be stored: "+directory);
+		analysisStart("numbers1.txt", directory); // (fileName, directory)	
+		}	
+		
+		catch (IOException e) {}
+		
+		// trying conversion of first URL
+		try { BufferedReader reader = nr.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
+		while ((line = reader.readLine()) != null) {
+			analyseData(line); // analyse lines, check for comments etc.
+		}
+		}
+		
 		catch (IOException e) {}
 		
 		
-		
-	}
-
 	
 
+
+
+
 }
+}
+
