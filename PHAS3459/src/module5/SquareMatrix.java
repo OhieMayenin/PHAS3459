39c5742e3aa8bbf60f9f0babae104022cc1ca3a9
Ohie Mayenin <zcapoma@ucl.ac.uk>
2017-11-02 11:47:45 +0000

    Module 5 SquareMatrix and test, add & subtract methods complete.

--- PHAS3459/src/module5/SquareMatrix.java
+++ PHAS3459/src/module5/SquareMatrix.java
@@ -9,6 +9,7 @@ public class SquareMatrix {
 
 	double[][] elements;
 
+	// constructor
 	public SquareMatrix (double[][] elementsIn) throws Exception {
 		elements = elementsIn;
 		this.numOfRows =  elements.length;
@@ -19,6 +20,7 @@ public class SquareMatrix {
 		}
 	}
 
+	// methods
 	public String toString () {
 		int i; // rows
 		int j; //columns
@@ -29,15 +31,98 @@ public class SquareMatrix {
 
 			double[] row_i = new double [numOfColumns];
 			row_i = elements[i];
-			
+
 			// counting elements in each row
 			for (j = 0; j < this.numOfColumns; j++) {
 				double doubElement = row_i[j];
 				stringBuilder.append(doubElement+", ");
 			}
 			stringBuilder.append("\n");
-			}
+		}
 		String str = "{"+ stringBuilder.toString() +"}";
 		return str;
 	}
+
+
+	public SquareMatrix IdentityMatrix (int size) {
+		int element;
+		SquareMatrix id_matrix;
+		
+		double [][] id = new double [size][size];
+		for (element = 0; element < size; element++) {
+			id[element][element] = 1;
+		}
+
+		try {
+			id_matrix = new SquareMatrix(id);	
+		}
+
+		catch (Exception e) {}
+		return id_matrix;
+	}
+
+
+	public boolean equals(Object obj) {
+		boolean ans = true;
+		if (obj == this) {ans = true;}
+		else if ((obj == null) || obj.getClass() != this.getClass()) {ans = false;}
+		return ans;
+	}
+
+
+	public static SquareMatrix add(SquareMatrix sm1, SquareMatrix sm2) throws Exception {
+		if ((sm1.numOfRows != sm2.numOfRows) || (sm1.numOfColumns != sm2.numOfColumns)) {
+			throw new Exception("ERROR: Cannot add square matrices of different sizes.");
+		}
+
+		double [][] sumMatrix = new double [sm1.numOfRows][sm1.numOfColumns];
+		int i = 0;
+		int j = 0;
+		
+		for (i = 0; i < sm1.numOfRows; i++ ) {
+			sumMatrix[i][j] = sm1.elements[i][j] + sm2.elements[i][j];
+			
+			for (j = 0; j < sm1.numOfColumns; j++) {
+				sumMatrix[i][j] = sm1.elements[i][j] + sm2.elements[i][j];
+			}
+			}
+
+		SquareMatrix sumSqMatrix = new SquareMatrix (sumMatrix);
+		return sumSqMatrix;
+	}
+
+
+
+
+public static SquareMatrix subtract (SquareMatrix sm1, SquareMatrix sm2) throws Exception {
+	if ((sm1.numOfRows != sm2.numOfRows) || (sm1.numOfColumns != sm2.numOfColumns)) {
+		throw new Exception("ERROR: Cannot add square matrices of different sizes.");
+	}
+
+	double [][] subtractMatrix = new double [sm1.numOfRows][sm1.numOfColumns];
+	int i = 0;
+	int j = 0;
+	
+	for (i = 0; i < sm1.numOfRows; i++ ) {
+		subtractMatrix[i][j] = sm1.elements[i][j] - sm2.elements[i][j];
+		
+		for (j = 0; j < sm1.numOfColumns; j++) {
+			subtractMatrix[i][j] = sm1.elements[i][j] - sm2.elements[i][j];
+		}
+		}
+
+	SquareMatrix subtractSqMatrix = new SquareMatrix (subtractMatrix);
+	return subtractSqMatrix;
+}
+
+
+
+public static SquareMatrix multiply(SquareMatrix sm1, SquareMatrix sm2) throws Exception {}
+	if (sm1.numOfRows != sm2.numOfColumns || sm1.numOfColumns != sm2.numOfRows) {
+		throw new Exception("ERROR: The matrices inputted cannot be multiplied due to their sizes."
+		+ " The number of columns of one must equal the number of rows of the other, or vice versa.");
+	}
+	double [][] productMatrix = new double [sm1.numOfRows][sm1.numOfColumns];
+	int i = 0;
+	int j = 0;
 }
--- PHAS3459/src/module5/TestSquareMatrix.java
+++ PHAS3459/src/module5/TestSquareMatrix.java
@@ -3,6 +3,11 @@ package module5;
 public class TestSquareMatrix {
 
 	public static void main(String[] args) {
+		SquareMatrix A;
+		SquareMatrix B;
+		SquareMatrix C;
+		SquareMatrix D;
+		
 		 double [][] a = { {1 , 2 , 0},
 		 		   		   {0 , 2 , 0},
 		 		   		   {-2 , 0 , 1} } ;
@@ -17,26 +22,28 @@ public class TestSquareMatrix {
 		 double [][] d = { {-2 , 3},
 		 		   		   {3 , -4} } ;
 		 
-		try { SquareMatrix A = new SquareMatrix (a);
+		try { A = new SquareMatrix (a);
 		System.out.println("Square Matrix A: \n " +A + "\n");
-		}
-		catch (Exception e) {}
 		
-		try { SquareMatrix B = new SquareMatrix (b);
+		B = new SquareMatrix (b);
 		System.out.println("Square Matrix B: \n " +B+ "\n");
-		}
-		catch (Exception e) {}
-		
-		try { SquareMatrix C = new SquareMatrix (c);
+			
+		C = new SquareMatrix (c);
 		System.out.println("Square Matrix C: \n " +C+ "\n");
-		}
-		catch (Exception e) {}
-		
-		try { SquareMatrix D = new SquareMatrix (d);
+			
+		D = new SquareMatrix (d);
 		System.out.println("Square Matrix D: \n " +D+ "\n");
 		}
+		
 		catch (Exception e) {}
 		
-	}
+		
+		// calculating sums
+		try {
+		SquareMatrix sumAB = SquareMatrix.add(A, B);
+		System.out.println(sumAB);
+		}
+		catch (Exception e) {}
+		}
 
 }
