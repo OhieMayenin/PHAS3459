6c3ab31bb695fc0eb983098bb2919971754960dc
Ohie Mayenin <zcapoma@ucl.ac.uk>
2017-11-10 13:24:34 +0000

    Module 5 first draft of DataAnalysis class with working dataFromURL

--- PHAS3459/src/module5/DataAnalysis.java
+++ PHAS3459/src/module5/DataAnalysis.java
@@ -1,16 +1,46 @@
 package module5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class DataAnalysis {
-	
-	
+
+	// converts URL name string to BufferedReader
+	public static ArrayList<Double> dataFromURL(String urlName) throws IOException {
+		ArrayList<Double> data = new ArrayList<Double>();
+		
+		URL u = new URL(urlName); // convert string to URL
+		InputStream is_url = u.openStream(); // inputs URL as bytes
+		InputStreamReader isr_url = new InputStreamReader(is_url); // wrap input stream
+		BufferedReader url = new BufferedReader(isr_url); // reads large chunk of data into memory
+		Scanner s = new Scanner(url);
+		
+		while (s.hasNext()) {
+			double num = Double.parseDouble(s.next()); // parse string from resource as double
+			data.add(num);
+			System.out.println(num); // print this double
+		}
+		return data;
+	}
 	
 	
 	
 	
 	
 	public static void main(String[] args) {
-		
-
+		String urlName = "http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-xy.txt";
+		try {
+			ArrayList<Double> dataList = dataFromURL(urlName);
+			System.out.println(dataList);
+		}
+		catch(IOException e){System.out.println("ERROR: IOException caught.");}
+	
 	}
 
 }
