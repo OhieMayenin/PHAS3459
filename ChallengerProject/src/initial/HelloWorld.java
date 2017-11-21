ff906ad02e6cffe5663d3779e28d943f6388afba
Ohie Mayenin <Ohie Mayenin@Mayenin-PC.home>
2017-11-21 18:09:15 +0000

    After some time, HelloWorld class after push config

--- ChallengerProject/src/initial/HelloWorld.java
+++ ChallengerProject/src/initial/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World!" +"\n");
 		System.out.println("Did you see what I done" +"\n" + "Came in the black Benz, left in the white one...");
 
+		System.out.println("\n" + "Some time later...");
 	}
 
 }
