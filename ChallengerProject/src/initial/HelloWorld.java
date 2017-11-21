63bac3bbd52ef1d1f9fc64c8880122b667e4dba0
Ohie Mayenin <Ohie Mayenin@Mayenin-PC.home>
2017-11-21 16:53:38 +0000

    Initial commit - working HelloWorld class.

---
+++ ChallengerProject/src/initial/HelloWorld.java
@@ -0,0 +1,11 @@
+package initial;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!" +"\n");
+		System.out.println("Did you see what I done" +"\n" + "Came in the black Benz, left in the white one...");
+
+	}
+
+}
