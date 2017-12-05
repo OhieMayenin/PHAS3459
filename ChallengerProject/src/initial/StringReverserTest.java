12d3cc6f15e59805817786ad8fd0f66cfba89698
Ohie Mayenin <Ohie Mayenin@Mayenin-PC.home>
2017-12-05 16:47:03 +0000

    StringReverserTest class late commit - working as intended

--- ChallengerProject/src/initial/StringReverserTest.java
+++ ChallengerProject/src/initial/StringReverserTest.java
@@ -8,7 +8,7 @@ public class StringReverserTest {
 	
 	public static void main(String[] args) {
 		String string1 = "racecar";
-		String string2 = "You are an incorrigible delinquent at times...";
+		String string2 = "Come to Queensbridge.";
 		String string3 = "2+2=4, minus 1 that's 3 - Quick Maths.";
 		
 			// static reverser
