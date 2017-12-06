ba8ef6724824daa4254d7f0af7a7dce339b920af
Ohie Mayenin <zcapoma@ucl.ac.uk>
2017-12-06 13:21:37 +0000

    Module 8 working prime number task

--- PHAS3459/src/module8/PrimeNumberTask.java
+++ PHAS3459/src/module8/PrimeNumberTask.java
@@ -5,32 +5,36 @@ import java.util.ArrayList;
 public class PrimeNumberTask implements Runnable {
 
 	public void run () {
-		int integer;
+		int integer = 2;
 		int i;
 		boolean isPrime = true;
 		ArrayList<Integer> primeNumbers = new ArrayList<Integer>(); // ArrayList to hold prime numbers
 		int maxInt = Integer.MAX_VALUE;
-		
+
 		// loop checks if time is prime number
-		while (Thread.currentThread().isInterrupted() ==false) {
-			for (integer = 2; integer < maxInt; integer++) {
-				isPrime = true;
 
-				for (i = 2; i < integer; i++) {
-					if (integer % i == 0) {
-						isPrime = false;
-						break;
-						//i = integer;
-					}
-				}
-
-				if (isPrime == true) {
-					primeNumbers.add(integer); // add prime number to ArrayList
-				}
-				
+		while (Thread.currentThread().isInterrupted() == false) {
+			isPrime = true;
+			if (integer == 2) {
+				primeNumbers.add(integer);
+				integer++;
+				continue;
 			}
-			
+			for (i = 2; i < integer; i++) {
+				if (integer % i == 0) {
+					isPrime = false;
+					break;
+					//i = integer;
+				}
+			}
+
+			if (isPrime == true) {
+				primeNumbers.add(integer); // add prime number to ArrayList
+			}
+			integer++;
+
 		}
 		System.out.println(primeNumbers);
+
 	}
 }
