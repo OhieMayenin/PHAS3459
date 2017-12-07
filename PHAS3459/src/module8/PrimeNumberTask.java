6d55d778797ef9740a026ad42fea8700edf08a57
Ohie Mayenin <zcapoma@ucl.ac.uk>
2017-12-07 11:44:41 +0000

    Prime Number Task working and printing relevant statistics

--- PHAS3459/src/module8/PrimeNumberTask.java
+++ PHAS3459/src/module8/PrimeNumberTask.java
@@ -9,7 +9,7 @@ public class PrimeNumberTask implements Runnable {
 		int i;
 		boolean isPrime = true;
 		ArrayList<Integer> primeNumbers = new ArrayList<Integer>(); // ArrayList to hold prime numbers
-		int maxInt = Integer.MAX_VALUE;
+		int lastInt = 0;
 
 		// loop checks if time is prime number
 
@@ -23,6 +23,7 @@ public class PrimeNumberTask implements Runnable {
 			for (i = 2; i < integer; i++) {
 				if (integer % i == 0) {
 					isPrime = false;
+					lastInt = integer;
 					break;
 					//i = integer;
 				}
@@ -30,11 +31,16 @@ public class PrimeNumberTask implements Runnable {
 
 			if (isPrime == true) {
 				primeNumbers.add(integer); // add prime number to ArrayList
+				lastInt = integer;
 			}
 			integer++;
 
 		}
-		System.out.println(primeNumbers);
+		//System.out.println(primeNumbers);
+		int totalPrimes = primeNumbers.size();
+		System.out.println("The total number of prime numbers: " +totalPrimes);
+		System.out.println("The largest prime number: " + primeNumbers.get(totalPrimes-1));
+		System.out.println("The largest integer checked so far: " + lastInt);
 
 	}
 }
